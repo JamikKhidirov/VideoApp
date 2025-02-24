b6a3cc5469f3ff8e698e2ddbd4777f3631f36239
Reysl <rasulnasaraliev@gmail.com>
2025-02-24 07:16:21 +0300

    Theme styling

--- app/src/main/java/com/reysl/videoapp/ui/theme/Color.kt
+++ app/src/main/java/com/reysl/videoapp/ui/theme/Color.kt
@@ -2,10 +2,10 @@ package com.reysl.videoapp.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val primaryColor = Color(0xFFB3F77D)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val backgroundColorLight = Color(0xFFFFFFFF)
+val onBackgroundLight = Color(0xFF141413)
+
+val backgroundColorDark = Color(0xFF141413)
+val onBackgroundDark = Color(0xFFF4F4F0)
